1b1921c6faee39f67eb78a8ca72c3e43a79e0424
Jckhe <iamjckhe@gmail.com>
2022-11-09 11:43:39 -0800

    added footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,5 @@
 import logo from '../assets/quell-logo.png';
+import { Dispatch, useEffect, useState, SetStateAction } from 'react';
 import quellBirdIcon from '../assets/images/quell_logos/QUELL-quail only.svg';
 import '../stylesheets/Navbar.css';
+
