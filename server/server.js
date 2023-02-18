6f89b0c1ee0076f2d7f1c31efb0e9ed386139008
Garik Asplund <Chris@MacBook-Air-140.home>
2023-02-17 16:34:05 -0800

    added endroute and global error handler

--- server/server.js
+++ server/server.js
@@ -43,6 +43,18 @@ app.use('/redis', quellCache.getRedisInfo({
   getValues: true
 }));
 
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
